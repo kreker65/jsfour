dbb86803abcbf8647f1bcda76337c0e8f476cde9
Roman Zholnovych <chickaroman@gmail.com>
2019-02-06 16:55:29 +0000

    jsfour

--- js/index.js
+++ js/index.js
@@ -59,3 +59,18 @@ const clockContainer = document.querySelector('.clock');
 setInterval(function() {
     clockContainer.innerText = (new Date()).toLocaleTimeString();
 }, 10);
+
+
+const display = document.querySelector('.calculator input[name=display]');
+
+document.querySelectorAll('.digits, .opers')
+ .forEach( btn => btn.addEventListener('click', digitOperPressed) );
+ 
+function digitOperPressed(e) {
+    display.value += e.target.innerText;
+}
+
+document.querySelector('.result')
+    .addEventListener('click', function(){
+        display.value = eval(display.value);       
+    })
